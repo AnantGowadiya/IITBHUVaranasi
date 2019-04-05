346ee774b450a4bb1ffa18af9106b3387de736d5
Anant Gowadiya <anant.gowadiya.mst14@itbhu.ac.in>
2019-04-05 19:16:43 +0530

    splash screen animation

--- app/src/main/java/com/example/anant/iitbhuvaranasi/MainActivity.java
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.anant.iitbhuvaranasi;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         //Splash Screen
-        new Handler().postDelayed(new Runnable() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.animated_logo);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
                 finish();
             }
-        }, 2000);
+        });
+
+        videoView.start();
     }
 }
