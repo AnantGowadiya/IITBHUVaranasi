1557680daf7fda2d866fdaad826935b2db009e55
ushmitapareek <pareek.ushmita@gmail.com>
2019-05-19 16:08:53 +0530

    share button created

--- app/src/main/java/com/example/anant/iitbhuvaranasi/ClubFeed.java
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/ClubFeed.java
@@ -7,36 +7,57 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class ClubFeed extends AppCompatActivity {
+public class ClubFeed extends AppCompatActivity implements View.OnClickListener {
     TextView content;
-    ImageButton plus, minus;
+    ImageButton expanddes, contractdes,clock, share ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_feed);
-        Intent intent=getIntent();
-        content = (TextView) findViewById(R.id.content);
-        plus = (ImageButton) findViewById(R.id.plus);
-        minus = (ImageButton) findViewById(R.id.minus);
+        Intent intent = getIntent();
+        content = findViewById(R.id.content);
+        expanddes = (ImageButton) findViewById(R.id.plus);
+        expanddes.setOnClickListener(this);
+        contractdes = (ImageButton) findViewById(R.id.minus);
+        contractdes.setOnClickListener(this);
+        clock=(ImageButton)findViewById(R.id.clock);
+        clock.setOnClickListener(this);
+        share=(ImageButton)findViewById(R.id.share);
+        share.setOnClickListener(this);
         content.setMaxLines(0);
 
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                plus.setVisibility(View.GONE);
-                minus.setVisibility(View.VISIBLE);
-                content.setMaxLines(Integer.MAX_VALUE);
-            }
+    }
 
-        });
-        minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                minus.setVisibility(View.GONE);
-                plus.setVisibility(View.VISIBLE);
+    @Override
+    public void onClick(View v) {
+        //default method for handling onClick events
+        switch (v.getId()) {
+
+            case R.id.plus:
+                expanddes.setVisibility(View.GONE);
+                contractdes.setVisibility(View.VISIBLE);
+                content.setMaxLines(Integer.MAX_VALUE);
+                break;
+
+            case R.id.minus:
+                contractdes.setVisibility(View.GONE);
+                expanddes.setVisibility(View.VISIBLE);
                 content.setMaxLines(0);
-            }
+                break;
+
+            case R.id.share:
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Here is the share content body";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                break;
+
+            default:
+                break;
         }
-        );}}
+    }
+}
